5a6bea2f5d22d29ba70f356ac324dac7140a56f3
Jeremy Kent <jeremy@rehabs.com>
2016-06-06 21:35:20 -0700

    add image preview

--- public/client.js
+++ public/client.js
@@ -1,38 +1,51 @@
 $(function () {
-  $.getJSON({
-              url : 'clippets',
-            })
-   .then(function ( data ) {
+  $.getJSON({ url : 'clippets' })
+  .then(function ( data ) {
+    $('#clippets').html('');
+    var seen = {};
+    data.forEach(function ( item ) {
+      console.log(item);
+      var fingerprint = (item.filename || item.content) + item.md5;
+      if ( seen[ fingerprint ] ) { return; }
+      seen[ fingerprint ] = true;
 
-     $('#clippets').html('');
-     var seen = {};
-
-     data.forEach(function ( item ) {
-
-       console.log(item);
-
-       var fingerprint = (item.filename || item.content) + item.md5;
-       if ( seen[ fingerprint ] ) {
-         return;
-       }
-       seen[ fingerprint ] = true;
-
-       switch ( item.type ) {
-         case 'image/jpeg':
-           $('#clippets')
-             .append(
-               $('<div class="clippet"></div>')
-                 .append(
-                   $('<img>')
-                     .attr('src', 'imgfile/' + item._id)
-                     .attr('type', item.type)
-                     .attr('alt', item.filename)
-                     .attr('height', 100)
-                     .attr('width', 100)
-                 )
-             );
-           break;
-       }
-     });
-   });
+      switch ( item.type ) {
+        case 'image/jpeg':
+          var clip = sprintf('<div class="clippet"><img src="%s" type="%s" alt="%s" height=%d width=%d></div>',
+            'imgfile/' + item._id, item.type, item.filename, 100, 100);
+          $('#clippets').append($(clip));
+          break;
+      }
+    });
+  });
 });
+
+(function(previewState){
+  function showPreview(imgUrl, keep = false){
+    if(previewState === 'off' || keep){
+      previewState = keep ? 'keep' : 'moment';
+      $('#clippets-col').removeClass('col-xs-12').addClass('col-xs-3');
+      $('#preview-col').show().find('img').attr('src', imgUrl);    
+    }
+  }
+
+  function hidePreview(force = false){
+    if(previewState === 'moment' || (previewState === 'keep' && force)){
+      previewState = 'off';
+      $('#clippets-col').addClass('col-xs-12').removeClass('col-xs-3');
+      $('#preview-col').hide();    
+    }
+  }
+
+  $('#clippets').on('mouseenter', 'img', function( event ) {
+    showPreview($(this).attr('src'));
+  }).on('mouseleave', 'img', function( event ) {
+    hidePreview();  
+  }).on('click', 'img', function( event ) {
+    showPreview($(this).attr('src'), true);
+  });
+
+  $('#preview').on('click', 'img', function( event ) {
+    hidePreview(true);
+  });
+})('off');
--- server.js
+++ server.js
@@ -10,8 +10,6 @@ var upload     = multer({
   dest: 'uploads/tmp/'
 })
 
-var db_uri = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
-
 app.use(express.static('public'));
 app.use(bodyParser.json());        // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({    // to support URL-encoded bodies
@@ -25,107 +23,86 @@ function errorer(response) {
   }
 }
 
-var pDb = mongodb.MongoClient.connect(db_uri, { promiseLibrary: Promise });
+var db_uri = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
+var pDb    = mongodb.MongoClient.connect(db_uri, { promiseLibrary: Promise });
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('health', function (request, response) {
-  response.sendStatus(200);
-});
-
-pCollArr = pDb
-  .then(function (db) {
-    return db.listCollections();
-})
-  .then(function (col) {
-    return col.toArray();
-});
-
-pCollStuff = pDb
-  .then(function (db) {
-    return db.collection('stuff');
-});
-
-app.get('/db', function (request, response) {
-  pCollArr
-    .then(function (arr) {
-      var resp = JSON.stringify(arr);
-      response.send(resp);
-  });
+pCollStuff = pDb.then(function (db) {
+  return db.collection('stuff');
 });
 
 app.post(
   "/file-upload", 
   upload.single('file'), 
   function (request, response) {
-    console.log('upload requested');
     pCollStuff
-      .then(function (collStuff) {
-        console.log('post', request.body);
-        var original = request.file.originalname;
-        var filename = request.file.destination + request.file.filename;
-        var content  = fs.readFileSync(filename);
-        var hash     = md5(content);
-        var doc      = {
-            'data'    : content,
-            'md5'     : hash,
-            'type'    : request.file.mimetype,
-            'filename': original
-        };
+    .then(function (collStuff) {
+      console.log('post', request.body);
+      var original = request.file.originalname;
+      var filename = request.file.destination + request.file.filename;
+      var content  = fs.readFileSync(filename);
+      var hash     = md5(content);
+      var doc      = {
+        'data'    : content,
+        'md5'     : hash,
+        'type'    : request.file.mimetype,
+        'filename': original
+      };
 
-        collStuff.insert(doc)
-          .then(function (resp) { // insert, update, find, drop
-            var id = resp.ops[0]._id;
-              console.log('added', id);
-              return id;
-        })
-          .catch(function (err) {
-            if (err.code === 11000) {
-              console.log('handle duplicate');
-              return collStuff
-                .findOne({ md5: hash, filename: original })
-                .then(function (matched) {
-                  console.log('already had', matched._id);
-                });
-            }
-            throw err;
-        })
-          .catch(errorer(response))
-          .finally(function(){
-            fs.unlink(request.file.destination + request.file.filename);
-        });
+      collStuff.insert(doc)
+      .then(function (resp) { // insert, update, find, drop
+        var id = resp.ops[0]._id;
+          console.log('added', id);
+          return id;
+      })
+      .catch(function (err) {
+        if (err.code === 11000) {
+          console.log('handle duplicate');
+          return collStuff
+          .findOne({ md5: hash, filename: original })
+          .then(function (matched) {
+            console.log('already had', matched._id);
+          });
+        }
+        throw err;
+      })
+      .catch(errorer(response))
+      .finally(function(){
+        fs.unlink(request.file.destination + request.file.filename);
+      });
     }).catch(errorer(response));
   response.sendStatus(200);
 });
 
 app.get('/clippets', function (request, response) {
-    pCollStuff
-      .then(function (collStuff) {
-        return collStuff
-          .find({}, {data: 0})
-          .sort({_id: -1})
-          .limit(10)
-          .toArray();
-    })
-      .then(function (arr) {
-        response.json(arr);
-    })
-      .catch(errorer(response));
+  pCollStuff
+  .then(function (collStuff) {
+    return collStuff
+      .find({}, { data: 0 })
+      .sort({ _id: -1 })
+      .limit(10)
+      .toArray();
+  })
+  .then(function (arr) {
+    return response.json(arr);
+  })
+  .catch(errorer(response));
 });
 
 app.get('/imgfile/:id', function (request, response) {
-    pCollStuff
-      .then(function (collStuff) {
-        var id = mongodb.ObjectId(request.params.id);
-        return collStuff.findOne({ _id: id });
-    })
-      .then(function (doc) {
-        response.set('Content-Type', doc.type);
-        response.send(doc.data.buffer);
-    })
-      .catch(errorer(response));
+  pCollStuff
+  .then(function (collStuff) {
+    var id = mongodb.ObjectId(request.params.id);
+    return collStuff.findOne({ _id: id });
+  })
+  .then(function (doc) {
+    response.set('Content-Type', doc.type);
+    response.send(doc.data.buffer);
+  })
+  .catch(errorer(response));
 });
 
 // listen for requests :)
