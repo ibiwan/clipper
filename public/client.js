cdf3c3509bb98d53842cad1edb6a4716108868d8
Jeremy Kent <jeremy@rehabs.com>
2016-06-14 00:23:29 -0700

    use handlebars and fix warnings

--- public/client.js
+++ public/client.js
@@ -1,115 +1,109 @@
-var seen = {};
-var clearClips = function(){
+var templates = {};
+[ 'tag_row', 'tag_btn', 'clippet' ].forEach(function ( t ) {
+  var template   = $('#' + t).html();
+  templates[ t ] = template;
+  Mustache.parse(template);
+});
+
+var seen       = {};
+var clearClips = function () {
   seen = {};
   $('#clippets').html('');
 };
 
-function addImageClip(_id, type, filename, md5str, tags=[]){
+function addImageClip( _id, type, filename, md5str, tags ) {
   var fingerprint = filename + md5str;
 
-	if(seen[fingerprint]){
-    $(seen[fingerprint]).remove();
+  if ( seen[ fingerprint ] ) {
+    $(seen[ fingerprint ]).remove();
   }
 
-  var tagset = $('<div class="tags row btn-toolbar"></div>');
-  for(var i in tags){
-    tagset.append(
-      $('<button type="button" class="tag btn btn-small btn-default"></button>')
-        .append(
-          $('<span></span').text(tags[i] + '\xa0')
-        )
-        .append(
-          $('<i class="delete glyphicon glyphicon-remove-sign color-grey pull-right"></i>')
-        )
-   );
+  var tagset = $(Mustache.render(templates.tag_row));
+  for ( var i in tags ) {
+    tagset.append(Mustache.render(templates.tag_btn, { tag : tags[ i ] }));
   }
 
-  var clippet = $('<div class="clippet row"></div>')
-    .append(
-      $('<div class="thumb col-xs-3"></div>')
-        .append(
-          $('<img class="img-responsive">')
-            .attr('src', 'imgfile/' + _id)
-            .attr('type', type)
-            .attr('alt', filename)
-        ), 
-      $('<div class="tagrow col"></div>').append(tagset)
-    )
-    .data('_id', _id)
-  ;
+  var clippet         = Mustache.render(templates.clippet, {
+    _id : _id, type : type, filename : filename
+  });
+  clippet             = $(clippet);
+  seen[ fingerprint ] = clippet;
+  clippet.find('.tagrow').append(tagset);
 
-  seen[fingerprint] = clippet;
   $('#clippets').append(clippet);
 }
 
-function addClip(item){
-    switch ( item.type ) {
-      case 'image/jpeg':
-      case 'image/png':
-        addImageClip(item._id, item.type, item.filename, item.md5, item.tags);
-        break;
-    }
+function addClip( item ) {
+  switch ( item.type ) {
+    case 'image/jpeg':
+    case 'image/png':
+      addImageClip(item._id, item.type, item.filename, item.md5, item.tags);
+      break;
+    default:
+      throw "couldn't handle type: " + item.type;
+      break;
+  }
 }
 
-function deleteTag(_id, tag){
-  $.getJSON({url:'tag/delete/'+_id+'/'+tag})
-  .then(function(data){
-    addClip(data);
-  });
+function deleteTag( _id, tag ) {
+  $.getJSON({ url : 'tag/delete/' + _id + '/' + tag })
+   .then(function ( data ) {
+     addClip(data);
+   });
 }
 
-function getList(){
+function getList() {
   $.getJSON({ url : 'clippets' })
-  .then(function ( data ) {
-		clearClips();
-    data.forEach(addClip);
-  });
+   .then(function ( data ) {
+     clearClips();
+     data.forEach(addClip);
+   });
 }
 
 $(getList);
 
 Dropzone.options.drop = {
-  init: function () {
+  init : function () {
     this.on("complete", getList);
   }
 };
 
-(function(previewState){
-  function showPreview(imgUrl, keep = false){
-    if(previewState === 'off' || keep){
+(function ( previewState ) {
+  function showPreview( imgUrl, keep ) {
+    if ( previewState === 'off' || keep ) {
       previewState = keep ? 'keep' : 'moment';
       $('#clippets-col').removeClass('col-xs-12').addClass('col-xs-3');
       $('.thumb').removeClass('col-xs-3').addClass('col-xs-12');
-      $('#preview-col').show().find('img').attr('src', imgUrl);    
+      $('#preview-col').show().find('img').attr('src', imgUrl);
       $('.tags').hide();
     }
   }
 
-  function hidePreview(force = false){
-    if(previewState === 'moment' || (previewState === 'keep' && force)){
+  function hidePreview( force ) {
+    if ( previewState === 'moment' || (previewState === 'keep' && force) ) {
       previewState = 'off';
       $('#clippets-col').removeClass('col-xs-3').addClass('col-xs-12');
       $('.thumb').removeClass('col-xs-12').addClass('col-xs-3');
-      $('#preview-col').hide();    
+      $('#preview-col').hide();
       $('.tags').show();
     }
   }
 
-  $('#clippets').on('mouseenter', '.thumb img', function( event ) {
-    showPreview($(this).attr('src'));
-  }).on('mouseleave', 'img', function( event ) {
-    hidePreview();  
-  }).on('click', 'img', function( event ) {
+  $('#clippets').on('mouseenter', '.thumb img', function ( event ) {
+    showPreview($(this).attr('src'), false);
+  }).on('mouseleave', 'img', function ( event ) {
+    hidePreview(false);
+  }).on('click', 'img', function ( event ) {
     showPreview($(this).attr('src'), true);
   });
 
-  $('#preview').on('click', '.thumb img', function( event ) {
+  $('#preview').on('click', 'img', function ( event ) {
     hidePreview(true);
   });
 })('off');
 
-$(document).on('click', '.delete', function(){
-  var tag = $.trim( $(this).closest('.tag').find('span').text() );
-  var _id  = $(this).closest('.clippet').data('_id');
+$(document).on('click', '.delete', function () {
+  var tag = $.trim($(this).closest('.tag').find('span').text());
+  var _id = $(this).closest('.clippet').data('_id');
   deleteTag(_id, tag);
 });
--- server.js
+++ server.js
@@ -2,123 +2,128 @@ var Promise    = require("bluebird");
 var mongodb    = require('mongodb');
 var express    = require('express');
 var app        = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var fs         = require('fs');
 var md5        = require('md5');
-var multer     = require('multer')
+var multer     = require('multer');
 var upload     = multer({
-  dest: 'uploads/tmp/'
-})
+                          dest : 'uploads/tmp/'
+                        });
 
 app.use(express.static('public'));
 app.use(bodyParser.json());        // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({    // to support URL-encoded bodies
-  extended: true
-}));
+                                extended : true
+                              }));
 
-var db_uri = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
-var pDb    = mongodb.MongoClient.connect(db_uri, { promiseLibrary: Promise });
+var db_uri = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST + ':' +
+             process.env.DB_PORT + '/' + process.env.DB_NAME;
+var pDb    = mongodb.MongoClient.connect(db_uri, { promiseLibrary : Promise });
 
-app.get("/", function (request, response, next) {
+app.get("/", function ( request, response ) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-pCollStuff = pDb.then(function (db) {
+pCollStuff = pDb.then(function ( db ) {
   return db.collection('stuff');
 });
 
 app.post(
-  "/file-upload", 
-  upload.single('file'), 
-  function (request, response, next) {
+  "/file-upload",
+  upload.single('file'),
+  function ( request, response, next ) {
     pCollStuff
-    .then(function (collStuff) {
-      var original = request.file.originalname;
-      var filename = request.file.destination + request.file.filename;
-      var content  = fs.readFileSync(filename);
-      var hash     = md5(content);
-      var firstTag = original.toLowerCase().replace(' ', '-');
-      var doc      = {
-        'data'    : content,
-        'md5'     : hash,
-        'type'    : request.file.mimetype,
-        'filename': original,
-        'tags'    : [firstTag]
-      };
+      .then(function ( collStuff ) {
+        var original = request.file.originalname;
+        var filename = request.file.destination + request.file.filename;
+        var content  = fs.readFileSync(filename);
+        var hash     = md5(content);
+        var firstTag = original.toLowerCase().replace(' ', '-');
+        var doc      = {
+          'data' : content,
+          'md5' : hash,
+          'type' : request.file.mimetype,
+          'filename' : original,
+          'tags' : [ firstTag ]
+        };
 
-      collStuff.insert(doc)
-      .then(function (resp) { // insert, update, find, drop
-        var id = resp.ops[0]._id;
-          response.send({id:id, hash:hash});
-          return id;
-      })
-      .catch(function (err) {
-        if (err.code === 11000) {
-          return collStuff
-          .findOne({ md5: hash, filename: original })
-          .then(function (matched) {
-            response.send({id:matched._id, hash:matched.hash});
-          });
-        }
-        throw err;
-      })
-      .catch(next)
-      .finally(function(){
-        fs.unlink(request.file.destination + request.file.filename);
-      });
-    }).catch(next);
-});
+        collStuff.insert(doc)
+                 .then(function ( resp ) { // insert, update, find, drop
+                   var id = resp.ops[ 0 ]._id;
+                   response.send({ id : id, hash : hash });
+                   return id;
+                 })
+                 .catch(function ( err ) {
+                   if ( err.code === 11000 ) {
+                     return collStuff
+                       .find({ md5 : hash, filename : original })
+                       .toArray()
+                       .then(function ( matched ) {
+                         response.send({ id : matched._id, hash : matched.hash });
+                       });
+                   }
+                   throw err;
+                 })
+                 .catch(next)
+                 .finally(function () {
+                   fs.unlink(request.file.destination + request.file.filename);
+                 });
+      }).catch(next);
+  });
 
-app.get('/clippets', function (request, response, next) {
+app.get('/clippets', function ( request, response, next ) {
   pCollStuff
-  .then(function (collStuff) {
-    return collStuff
-      .find({}, { data: 0 })
-      .sort({ _id: -1 })
-      .limit(10)
-      .toArray();
-  })
-  .then(function (arr) {
-    return response.json(arr);
-  })
-  .catch(next);
-});
-
-app.get('/tag/delete/:_id/:tag', function(request, response, next){
-  pCollStuff
-  .then(function(collStuff){
-    var idObj = mongodb.ObjectId(request.params._id);
-    var queryDoc = {_id:idObj};
-    var updateDoc = {'$pullAll': { tags: [ request.params.tag ] } };
-    
-    collStuff
-    .update(queryDoc, updateDoc)
-    .then(function(result){
-      return collStuff.findOne(queryDoc, {data:0});
+    .then(function ( collStuff ) {
+      return collStuff
+        .find({}, { data : 0 })
+        .sort({ _id : -1 })
+        .limit(10)
+        .toArray();
     })
-    .then(function(arr){
-      console.log(arr);
+    .then(function ( arr ) {
       return response.json(arr);
-    });
-  })
-  .catch(next);
+    })
+    .catch(next);
 });
 
-app.get('/imgfile/:id', function (request, response, next) {
+app.get('/tag/delete/:_id/:tag', function ( request, response, next ) {
   pCollStuff
-  .then(function (collStuff) {
-    var id = mongodb.ObjectId(request.params.id);
-    return collStuff.findOne({ _id: id });
-  })
-  .then(function (doc) {
-    response.set('Content-Type', doc.type);
-    response.send(doc.data.buffer);
-  })
-  .catch(next);
+    .then(function ( collStuff ) {
+      var idObj     = mongodb.ObjectId(request.params._id);
+      var queryDoc  = { _id : idObj };
+      var updateDoc = { '$pullAll' : { tags : [ request.params.tag ] } };
+
+      collStuff
+        .update(queryDoc, updateDoc)
+        .then(function () {
+          return collStuff
+            .find(queryDoc, { data : 0 })
+            .toArray();
+        })
+        .then(function ( arr ) {
+          console.log(arr);
+          return response.json(arr[ 0 ]);
+        });
+    })
+    .catch(next);
+});
+
+app.get('/imgfile/:id', function ( request, response, next ) {
+  pCollStuff
+    .then(function ( collStuff ) {
+      var id = mongodb.ObjectId(request.params.id);
+      return collStuff.find({ _id : id }).toArray();
+    })
+    .then(function ( docs ) {
+      var doc = docs[0];
+      response.set('Content-Type', doc.type);
+      response.send(doc.data.buffer);
+    })
+    .catch(next);
 });
 
 listener = app.listen(3000, function () {
   console.log(
-    'Your app is listening on port ' 
-    + listener.address().port); 
+    'Your app is listening on port '
+    + listener.address().port);
 });
